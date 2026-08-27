05a8fe99c4e67e760f6eeff65750174377d9fa66
David Mahecha <david.mahecha@estudiantesunibague.edu.co>
2026-08-27 17:12:36 -0500

    Se creó el archivo multiplicacion.py donde se piden 2 numeros por consola y bota el resultado de los dosnúmeros multiplicados

---
+++ Multiplicacion.py
@@ -0,0 +1,5 @@
+numero1 = int(input("Ingrese el primer número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+resultado = numero1 * numero2
+print(f"El resultado de la multiplicación es: {resultado}")
+

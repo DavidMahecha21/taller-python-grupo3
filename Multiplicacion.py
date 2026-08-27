69a83be243d4028e15b478d3bfe1e0be5dff9b41
David Mahecha <david.mahecha@estudiantesunibague.edu.co>
2026-08-27 17:17:52 -0500

    metodo multiplicacion

--- Multiplicacion.py
+++ Multiplicacion.py
@@ -1,5 +1,4 @@
-numero1 = int(input("Ingrese el primer número: "))
-numero2 = int(input("Ingrese el segundo número: "))
-resultado = numero1 * numero2
-print(f"El resultado de la multiplicación es: {resultado}")
+def multiplicacion (a, b):
+
+    return a * b
 

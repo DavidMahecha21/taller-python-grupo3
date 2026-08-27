78a378dc278f14a93432c2eb10ad7a9327a2db16
JuanLopez2099 <juan.lopez1@estudiantesunibague.edu.co>
2026-08-27 17:25:06 -0500

    suma añadida

--- Calculadora.py
+++ Calculadora.py
@@ -1,4 +1,5 @@
 import diviision as dv
+import Suma as sm
 
 while True:
     print("\n--- Calculadora Básica ---")
@@ -10,8 +11,14 @@ while True:
     
     opcion = input("Elige una opción (1-5): ")
     
+    if(opcion == "1"):
+        num1 = int(input("Digite el primer numero: "))
+        num2 = int(input("Digite el segundo numero: "))
+        
+        print(sm.suma(num1, num2))
     if(opcion == "4"):
         num1 = int(input("Digite el primer numero: "))
         num2 = int(input("Digite el segundo numero: "))
         
         print(dv.division(num1, num2))
+    
